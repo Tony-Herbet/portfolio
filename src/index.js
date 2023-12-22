882912305247fdb6f76bf9dc88234b4ab2904bf0
Tony Herbet Le Faucheur <herbet.le.faucheur.tony@gmail.com>
2023-12-22 16:47:07 +0100

    Update react

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from "react-dom";
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './containers/App';
 import store from './store';
 
-// import './styles/reset.css';
 import './styles/style.css';
 
-const rootReactElement = (
+const rootReactElement = createRoot(document.getElementById("root"));
+
+rootReactElement.render(
   <Provider store={store}>
     <Router>
       <App />
     </Router>
   </Provider>
 );
-const target = document.getElementById('root');
-render(rootReactElement, target);

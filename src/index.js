73694bb39914645a599081b33228a450cde9ca2d
Tony Herbet Le Faucheur <herbet.le.faucheur.tony@gmail.com>
2024-01-23 17:01:26 +0100

    Add route

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from "react-dom/client";
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import App from './containers/App';
 import store from './store';
@@ -14,7 +14,9 @@ const rootReactElement = createRoot(document.getElementById("root"));
 rootReactElement.render(
   <Provider store={store}>
     <Router>
-      <App />
+      <Routes>
+        <Route path='*' Component={App}/>
+      </Routes>
     </Router>
   </Provider>
 );
